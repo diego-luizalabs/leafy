c9ea18308c8d5cab740f50a9da2a67f422ba04b0
Your Name <diego2.sa@luizalabs.com>
2025-10-20 08:51:24 -0300

    Initial Commit

---
+++ Leafy/LeafyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeafyApp.swift
+//  Leafy
+//
+//  Created by Diego Santos on 20/10/25.
+//
+
+import SwiftUI
+
+@main
+struct LeafyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
